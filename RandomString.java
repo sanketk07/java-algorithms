f5cf6e1d243665e3ff34070542e6bdf61c96f880
sanketk07 <sanketskumar@gmail.com>
2018-02-27 18:37:14 -0500

    Random String Generator

---
+++ RandomString.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.homework6.sanket;
+
+/**
+ * @author sanketkumar
+ *
+ */
+public class RandomString {
+
+	 public static void main(String[] args) {
+		 
+		 final String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		 
+		 final String lower = upper.toLowerCase();
+			
+		 System.out.println(lower);
+	 }
+
+}
