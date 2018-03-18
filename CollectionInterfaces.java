4c18ad8bc66e74d9eccaa7864816745f4b657065
sanketk07 <sanketskumar@gmail.com>
2018-03-17 21:58:11 -0400

    Indenting CollectionInterfaces

--- CollectionInterfaces.java
+++ CollectionInterfaces.java
@@ -4,7 +4,7 @@
 package com.questions.test;
 
 import java.util.ArrayList;
-import java.util.Collection;
+//import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
@@ -32,35 +32,35 @@ public class CollectionInterfaces {
 
 		ArrayList<String> list = new ArrayList<String>();
 		list.isEmpty();
-		
+
 		LinkedHashSet<String> lHashSet = new LinkedHashSet<String>();
 		lHashSet.isEmpty();
-		
+
 		TreeSet<String> tset = new TreeSet<String>();
 		tset.isEmpty();
-		
+
 		SortedSet<Integer> sortedSet = new TreeSet<Integer>();
 		sortedSet.isEmpty();
-		
+
 		Set<Integer> set = new HashSet<Integer>();
 		set.isEmpty();
-		
+
 		Map<Integer, String> map = new HashMap<Integer, String>();
 		map.isEmpty();
-		
+
 		Queue<String> q = new PriorityQueue<String>();
 		q.isEmpty();
 		q.peek();
-		
+
 		List<String> vector = new Vector<String>();
 		vector.isEmpty();
-		
+
 		List<String> linkedList = new LinkedList<String>();
 		linkedList.isEmpty();
-		
+
 		Queue<String> queue = new PriorityQueue<String>();
 		queue.isEmpty();
-		
+
 	}
 
 }
